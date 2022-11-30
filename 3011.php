1596c64ba70c109a9252ead0061b2fd5edf7b435
DEMETRIUS CARNEIRO MONTEIRO FERREIRA <68928535+demithehomie@users.noreply.github.com>
2022-11-30 11:55:11 -0300

    primeiro commit

---
+++ 3011.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+
+ACELERANDO O DESENVOLVIMENTO PELO TERMINAL
+
+mkdir - cria uma pasta
+type nul> "nome do arquivo.php" - cria um arquivo
+cd "nome da pasta" - entra numa pasta
+cd ../ - sai da pasta onde você está
+
+INTERVALO ATÉ 10:40
+
+
+*/
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+
